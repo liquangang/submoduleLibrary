7509da408c7e6ba66051e661b368b628867183d8
liquangang <liquangang@idstaff.com>
2017-04-10 11:14:01 +0800

    提交项目

---
+++ submoduleLibrary/submoduleLibrary.h
@@ -0,0 +1,19 @@
+//
+//  submoduleLibrary.h
+//  submoduleLibrary
+//
+//  Created by quangang on 2017/4/10.
+//  Copyright © 2017年 必要. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for submoduleLibrary.
+FOUNDATION_EXPORT double submoduleLibraryVersionNumber;
+
+//! Project version string for submoduleLibrary.
+FOUNDATION_EXPORT const unsigned char submoduleLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <submoduleLibrary/PublicHeader.h>
+
+
