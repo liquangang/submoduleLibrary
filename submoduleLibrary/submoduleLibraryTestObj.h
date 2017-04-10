33f14875b1de593ef0b8fab27c3595777b55632f
liquangang <liquangang@idstaff.com>
2017-04-10 11:27:43 +0800

    增加第一个测试方法

---
+++ submoduleLibrary/submoduleLibraryTestObj.h
@@ -0,0 +1,15 @@
+//
+//  submoduleLibraryTestObj.h
+//  submoduleLibrary
+//
+//  Created by quangang on 2017/4/10.
+//  Copyright © 2017年 必要. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface submoduleLibraryTestObj : NSObject
+
+- (void)submoduleLibraryTestObjFirstMethod;
+
+@end
---
+++ submoduleLibrary/submoduleLibraryTestObj.m
@@ -0,0 +1,19 @@
+//
+//  submoduleLibraryTestObj.m
+//  submoduleLibrary
+//
+//  Created by quangang on 2017/4/10.
+//  Copyright © 2017年 必要. All rights reserved.
+//
+
+#import "submoduleLibraryTestObj.h"
+
+@implementation submoduleLibraryTestObj
+
+- (void)submoduleLibraryTestObjFirstMethod{
+    NSLog(@"submoduleLibraryTestObj测试方法1");
+    NSLog(@"submoduleLibraryTestObj测试方法1");
+    NSLog(@"submoduleLibraryTestObj测试方法1");
+}
+
+@end
